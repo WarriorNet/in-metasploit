104ed274fe524c15589c259b8b763e425de2f827
WarriorNet <76886916+WarriorNet@users.noreply.github.com>
2021-01-02 19:29:06 -0300

    Create metasploit.sh

---
+++ metasploit.sh
@@ -0,0 +1,11 @@
+
+echo "instalando o metasploit"
+
+git clone https://github.com/verluchie/termux-metasploit
+cd termux-metasploit
+chmod 777 install.sh
+sh install.sh
+clear
+sleep 2
+echo "pronto"
+
